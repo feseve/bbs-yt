dcbf3bb8355883a6af0df58679b099dda90448bc
feseve <yujibp0809@gmail.com>
2023-03-02 01:13:04 +0900

    ログイン画面実装 バリデーション表示画面遷移なしに

--- public/signup_form.php
+++ public/signup_form.php
@@ -1,3 +1,12 @@
+<?php 
+    session_start();
+    $err = $_SESSION;
+
+    //セッション切断
+    $_SESSION = array();
+    session_destroy();
+?>
+
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -8,22 +17,34 @@
 </head>
 <body>
     <h2>ユーザー登録フォーム</h2>
-    <form action="registar.php" method="post">
+    <form action="registar.php" method="POST">
         <p>
-            <label for="usrname">名前：</label>
+            <label for="username">名前：</label>
             <input type="text" name="username">
+            <?php if (isset($err['username'])): ?>
+                <p><?php echo $err['username']; ?></p>
+            <?php endif; ?>
         </p>
         <p>
             <label for="email">メールアドレス：</label>
-            <input type="text" name="email">
+            <input type="email" name="email">
+            <?php if (isset($err['email'])): ?>
+                <p><?php echo $err['email']; ?></p>
+            <?php endif; ?>
         </p>
         <p>
             <label for="password">パスワード：</label>
             <input type="password" name="password">
+            <?php if (isset($err['password'])): ?>
+                <p><?php echo $err['password']; ?></p>
+            <?php endif; ?>
         </p>
         <p>
             <label for="password_conf">パスワード確認：</label>
             <input type="password" name="password_conf">
+            <?php if (isset($err['password_conf'])): ?>
+                <p><?php echo $err['password_conf']; ?></p>
+            <?php endif; ?>
         </p>
         <p>
             <input type="submit" value="登録">
